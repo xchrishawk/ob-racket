394c3ad087887a0e28a03e93630c3dc1d1fb2d70
George Mauer <gmauer@gmail.com>
2020-05-29 21:16:13 -0500

    proper indenttion

--- ob-racket.el
+++ ob-racket.el
@@ -55,38 +55,41 @@
   "Executes a Racket code block."
   ;; Round up the stuff we need
   (let* ((parsed-params (ob-racket--parse-params params))
-	 (expanded-body (org-babel-expand-body:racket body params))
-	 (result-type (nth 0 parsed-params))
-	 (lang (nth 1 parsed-params))
-	 (vars (nth 2 parsed-params))
-	 (temp-file (make-temp-file "ob-racket-")))
+         (expanded-body (org-babel-expand-body:racket body params))
+         (result-type (nth 0 parsed-params))
+         (lang (nth 1 parsed-params))
+         (vars (nth 2 parsed-params))
+         (requires-statements (nth 3 parsed-params))
+         (temp-file (make-temp-file "ob-racket-")))
     ;; Build script in temporary file
     (with-temp-file temp-file
       (cond
        ;; Results type is "value" - run in let form
        ((equal result-type 'value)
-	(let ((vars-string
-	      (mapconcat (lambda (var) (format "[%s (quote %s)]" (car var) (cdr var))) vars " ")))
-	  (insert (format "#lang %s\n(let (%s)\n%s)"
-			  lang
-			  vars-string
-			  expanded-body))))
+        (let ((vars-string
+               (mapconcat (lambda (var) (format "[%s (quote %s)]" (car var) (cdr var))) vars " ")))
+          (insert (format "#lang %s\n%s\n(let (%s)\n%s)"
+                          lang
+                          requires-statements
+                          vars-string
+                          expanded-body))))
        ;; Results type is "output" - run as script
        ((equal result-type 'output)
-	(let ((vars-string
-	       (mapconcat (lambda (var) (format "(define %s (quote %s))" (car var) (cdr var))) vars "\n")))
-	  (insert (format "#lang %s\n%s\n%s"
-			  lang
-			  vars-string
-			  body))))
+        (let ((vars-string
+               (mapconcat (lambda (var) (format "(define %s (quote %s))" (car var) (cdr var))) vars "\n")))
+          (insert (format "#lang %s\n%s\n%s\n%s"
+                          lang
+                          requires-statements
+                          vars-string
+                          body))))
        ;; Unknown result type??
        (t (error "Invalid result type: %s" result-type))))
     ;; Run script with Racket interpreter, delete temp file, and return output
     (with-temp-buffer
       (prog2
-	  (call-process org-babel-command:racket nil (current-buffer) nil temp-file)
-	  (buffer-string)
-	(delete-file temp-file)))))
+          (call-process org-babel-command:racket nil (current-buffer) nil temp-file)
+          (buffer-string)
+        (delete-file temp-file)))))
 
 (defun org-babel-prep-session:racket (session params)
   (error "Racket does not currently support sessions."))
